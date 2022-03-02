78a5cb3aa93e8379e2a59582526a921506a54800
thanh.hoang <thanh.hoang@finviet.com.vn>
2022-03-02 22:22:38 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const bootstrap = require('./lib/infrastructure/config/bootstrap');
-const app = require('./lib/infrastructure/webserver/server');
-const http = require('http');
-var debug = require('debug')('server:server');
+const httpServer = require('./lib/infrastructure/webserver/server');
 
 // Start the server
 const start = async () => {
@@ -11,70 +9,11 @@ const start = async () => {
   try {
     await bootstrap.init();
 
-    const port = normalizePort(process.env.PORT || '3333');
-    app.set('port', port);
+    const port = process.env.PORT || '3333';
 
-
-    var server = http.createServer(app);
-
-    server.listen(port);
-    server.on('error', onError);
-    server.on('listening', onListening);
-
-    function normalizePort(val) {
-      var port = parseInt(val, 10);
-
-      if (isNaN(port)) {
-        // named pipe
-        return val;
-      }
-
-      if (port >= 0) {
-        // port number
-        return port;
-      }
-
-      return false;
-    }
-
-
-    function onError(error) {
-      if (error.syscall !== 'listen') {
-        throw error;
-      }
-
-      var bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
-
-      // handle specific listen errors with friendly messages
-      switch (error.code) {
-        case 'EACCES':
-          console.error(bind + ' requires elevated privileges');
-          process.exit(1);
-          break;
-        case 'EADDRINUSE':
-          console.error(bind + ' is already in use');
-          process.exit(1);
-          break;
-        default:
-          throw error;
-      }
-    }
-
-    /**
-     * Event listener for HTTP server "listening" event.
-     */
-
-    function onListening() {
-      var addr = server.address();
-      var bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-      debug('Listening on ' + bind);
-    }
-
-    // await server.start();
+    httpServer.listen(port, () => {
+      console.log("WS running on port " + port);
+    })
 
   } catch (err) {
     console.log(err);
